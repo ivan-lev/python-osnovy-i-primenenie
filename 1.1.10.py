37f3a76a20255dee9a2752edc7d785e1fd21b7da
ivan-lev <fanatos@mail.ru>
2017-05-16 14:21:23 +0500

    Нашел более компактный способ решить задачу

--- 1.1.10.py
+++ 1.1.10.py
@@ -1,3 +1,7 @@
+# а вот как пишут код крутые ребята
+print(sum([int(input()) for i in range(int(input()))]))
+
+# для примера и сравнения оставил своё решение
 x = 0
 for i in range(int(input())):
     x += int(input())
