1fed1c7a1edb7871a35acafe44801ba2d22398a1
Ivan Lev <fanatos@mail.ru>
2017-05-20 15:05:32 +0500

    Решение к заданию 1.3.15

---
+++ 1.3.15.py
@@ -0,0 +1,11 @@
+def cnk(n, k):
+    if k == 0:
+        return 1
+    elif k > n:
+        return 0
+    else:
+        return cnk(n - 1, k) + cnk(n - 1, k - 1)
+
+n, k = map(int, input().split())
+
+print(cnk(n, k))
